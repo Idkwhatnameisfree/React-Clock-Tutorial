70382e9b124f1a31c331d528701953ffc28c06d2
Idkwhatnameisfree <giuliano.wendt@gmail.com>
2019-04-09 13:42:44 +0200

    Updates

---
+++ precache-manifest.6e2d8dae7894023dc27db7e2fc63fc4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Clock-Tutorial/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9edac55aec4850e35b85",
+    "url": "/React-Clock-Tutorial/static/js/runtime~main.1b0a89cb.js"
+  },
+  {
+    "revision": "e2f19216af0726d43864",
+    "url": "/React-Clock-Tutorial/static/js/main.a5ec5b28.chunk.js"
+  },
+  {
+    "revision": "72bf16a995fbfea566b2",
+    "url": "/React-Clock-Tutorial/static/js/2.e73f3848.chunk.js"
+  },
+  {
+    "revision": "e2f19216af0726d43864",
+    "url": "/React-Clock-Tutorial/static/css/main.98133600.chunk.css"
+  },
+  {
+    "revision": "5bfad35e0397aea4e18c029d7910ebe3",
+    "url": "/React-Clock-Tutorial/index.html"
+  }
+];
